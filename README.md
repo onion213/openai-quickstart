7da8e54276a77169ea117de719814de6777b304d
onion <kosukekito38@gmail.com>
2023-11-16 19:53:33 +0900

    initial commit: quickstart

---
+++ README.md
@@ -0,0 +1,5 @@
+# OpenAI Quickstart
+
+First contact with OpenAI API. Official quickstart and other explorations.
+
+reference: https://platform.openai.com/docs/quickstart?context=python
---
+++ openai_quickstart/my_first_request.py
@@ -0,0 +1,16 @@
+import dotenv
+from openai import OpenAI
+
+dotenv.load_dotenv()
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    #model="gpt-4-1106-preview",
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+      {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+    ]
+)
+
+print(completion.choices[0].message)
